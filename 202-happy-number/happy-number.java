da7746627bd6067d457f8fd6213db7ffbc0818ed
VISHAL A C <vishalchandrasekar2006@gmail.com>
2025-08-31 11:46:53 +0530

    Time: 1 ms (64.34%) | Memory: 40.5 MB (93.60%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int sumofdigits(int n){
+        int sum=0;
+         while(n>0){
+                int rem=n%10;
+                sum+=(rem*rem);
+                n=n/10;
+            }
+            return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        do{
+            slow=sumofdigits(slow);
+            fast=sumofdigits(fast);
+            fast=sumofdigits(fast);
+
+        }while(slow!=fast);
+        if(slow==1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
