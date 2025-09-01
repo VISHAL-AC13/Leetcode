699d88299d483b8b922b375f180e26e8ae142303
VISHAL A C <vishalchandrasekar2006@gmail.com>
2025-09-01 14:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (49.22%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int binaryGap(int n) {
+        int last_one=-1;
+        int curdist=0,dist=0;;
+        int curr=0,adj;
+        while(n>0){
+            if((n&1)==1){
+                if(last_one==-1){
+                    last_one=curr;
+                }
+                else{
+                    adj=curr;
+                    curdist=adj-last_one;
+                }
+                if(curdist>=dist){
+                    dist=curdist;
+                }
+                last_one=curr;
+            }
+            curr++;
+            n>>=1;
+        }
+        return dist;
+    }
+    
+}
