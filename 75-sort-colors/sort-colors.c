9f87b27a1a5632131934fcf88e83b2a1c57a8bd1
VISHAL A C <vishalchandrasekar2006@gmail.com>
2025-12-14 21:32:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.71%) - LeetSync

---
+++ 75-sort-colors/sort-colors.c
@@ -0,0 +1,11 @@
+void sortColors(int* nums, int numsSize) {
+    for(int i=0;i<numsSize;i++){
+        for(int j=0;j<numsSize-1-i;j++){
+            if(nums[j]>nums[j+1]){
+                int temp=nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+            }
+        }
+    }
+}
