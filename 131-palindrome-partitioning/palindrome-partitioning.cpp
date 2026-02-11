b17570fafc73dd2627e859f5dc8f6f22727a3176
VISHAL A C <vishalchandrasekar2006@gmail.com>
2026-02-11 12:06:45 +0530

    Time: 71 ms (21.97%) | Memory: 84.1 MB (19.31%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> temp;
+    bool ispalin(string s){
+        int i=0,j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    void func(int index,string s){
+        if(index==s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int j=index;j<s.size();j++){
+            string str=s.substr(index,j-index+1);
+            if(ispalin(str)){
+                    temp.push_back(str);
+                    func(j+1,s);
+                    temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        func(0,s);
+        return ans;   
+    }
+};
