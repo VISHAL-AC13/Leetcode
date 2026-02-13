7a392ef0d31a61f20baf800b736a52bacbc4cd0a
VISHAL A C <vishalchandrasekar2006@gmail.com>
2026-02-13 09:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (22.82%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int jumps=0;
+        int m=-1;
+        if(nums.size()==1) return true;
+        for(int i=0;i<nums.size()-1;i++){
+            m=max(m,nums[i]+i);
+            if(nums[i]==0&&m<=i){
+                return false;
+            }
+
+        
+        }
+        return true;
+    }
+};
