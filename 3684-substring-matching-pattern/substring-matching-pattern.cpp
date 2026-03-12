9aa01a3818ff8a00feea0fde41475b2a34dca525
VISHAL A C <vishalchandrasekar2006@gmail.com>
2026-03-12 20:56:05 +0530

    Time: 80 ms (6.89%) | Memory: 37.2 MB (5.10%) - LeetSync

---
+++ 3684-substring-matching-pattern/substring-matching-pattern.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+bool func(string s,string p,int i,int j){
+    if(j<0) return true;
+    if(i<0&&j>=0){
+        while(j>=0&&p[j]=='*') j--;
+        return j<0;
+    }
+    if(s[i]==p[j]){
+        return func(s,p,i-1,j-1);
+    }
+    else if(p[j]=='*'){
+        return func(s,p,i-1,j)||func(s,p,i,j-1);
+    }
+    else{
+        return false;
+    } 
+
+}
+    bool hasMatch(string s, string p) {
+        int n=s.size();
+        int m=p.size();
+        for(int i=0;i<n;i++){
+            if(func(s,p,i,m-1)) return true;
+        }
+        return false;
+    }
+};
